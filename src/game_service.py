b211166dec86adc83f36a0f0dadfb7c86404d43c
Diogovpam <53824716+Diogovpam@users.noreply.github.com>
2025-02-12 21:38:05 -0300

    feat: decouple and structure game service

--- src/game_service.py
+++ src/game_service.py
@@ -1,49 +1,47 @@
 from typing import Dict, Optional, List
 from fastapi import HTTPException
 from src.entities.request_models import CreateGameRequest, JoinGameRequest, StartGameRequest, SubmitMoveRequest
-from src.state_manager import GameStateManager
+from src.state_manager import GameStateManager, PlayerState
 from src.entities.entities import PlayerInfo, Factions
 
 
 class GameManager:
-
     def __init__(self):
-        self.games: Dict[str, List[GameStateManager, Optional[GameStateManager]]] = {}
-        self.player_moves: Dict[str, List[int]]
+        self.games: Dict[str, GameStateManager] = {}
 
     def create_game(self, request: CreateGameRequest):
+        """ Creates a new game with one player. """
         if request.game_id in self.games:
             raise HTTPException(status_code=400, detail="Game already exists")
 
         player_info = PlayerInfo(player_name=request.player_name, faction=Factions[request.faction.upper()])
-        self.games.update({
-            request.game_id: [GameStateManager(player_info)]
-        })
+        self.games[request.game_id] = GameStateManager(player_info)
 
         return {"message": "Game created", "game_id": request.game_id}
 
     def list_available_games(self):
-        return [game_id for game_id in self.games.keys() if len(self.games[game_id]) == 1]
+        return [game_id for game_id in self.games.keys() if self.games[game_id].opponent.name == "Opponent"]
 
     def join_game(self, request: JoinGameRequest):
+        """ Allows an opponent to join an existing game. """
         if request.game_id not in self.games:
             raise HTTPException(status_code=404, detail="Game not found")
 
         game = self.games[request.game_id]
-        if len(game) > 1:
+
+        # Ensure only one opponent joins
+        if game.opponent.name != "Opponent":
             raise HTTPException(status_code=400, detail="Game is already full")
 
-        opposing_faction = Factions.get_opposing_faction(game[0].player.faction)
-        game.append(
-            GameStateManager(
-                player_info=PlayerInfo(
-                    player_name=request.player_name,
-                    faction=opposing_faction
-                )
-            )
-        )
+        opposing_faction = Factions.get_opposing_faction(game.player.faction)
+        game.opponent = PlayerState(player_name=request.player_name, faction=opposing_faction)
 
         return {"message": f"{request.player_name} joined", "faction": opposing_faction.value}
 
     def submit_move(self, request: SubmitMoveRequest):
-        pass
+        """ Submits a move for a player and resolves the turn. """
+        if request.game_id not in self.games:
+            raise HTTPException(status_code=404, detail="Game not found")
+
+        game = self.games[request.game_id]
+        return game.submit_move(request.faction, request.move_index)
